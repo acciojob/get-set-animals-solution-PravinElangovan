586b93de73db2299ce4a4b74d0e1bb814ca0a49d
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-04-10 14:45:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 class Animal {
-  constructor(species) {
-    this._species = species;
+    constructor(species) {
+      this._species = species;
+    }
+  
+    get species() {
+      return this._species;
+    }
+  
+    makeSound() {
+      console.log("The " + this.species + " makes a sound");
+    }
   }
-
-  get species() {
-    return this._species;
+  
+  class Cat extends Animal {
+    purr() {
+      console.log("purr");
+    }
   }
-
-  makeSound() {
-    console.log("The " + this.species + " makes a sound");
+  
+  class Dog extends Animal {
+    bark() {
+      console.log("woof");
+    }
   }
-}
-
-class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
-}
-
-class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
-}
-
+  
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
